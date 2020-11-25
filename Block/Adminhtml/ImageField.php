bd7bf29cbafd4a59a7a8d4166ed38ac19f7b728b
Danilo Argentiero <argentiero.danilo@gmail.com>
2020-11-25 12:47:01 +0100

    init: first commit

---
+++ Block/Adminhtml/ImageField.php
@@ -0,0 +1,75 @@
+<?php
+/*
+ * Copyright © Websolute spa. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Websolute\WidgetImageChooser\Block\Adminhtml;
+
+use Magento\Backend\Block\Template;
+use Magento\Backend\Block\Template\Context as TemplateContext;
+use Magento\Backend\Block\Widget\Button;
+use Magento\Framework\Data\Form\Element\AbstractElement as Element;
+use Magento\Framework\Data\Form\Element\Factory as FormElementFactory;
+use Magento\Framework\Data\Form\Element\Text;
+use Magento\Framework\Exception\LocalizedException;
+
+class ImageField extends Template
+{
+    /**
+     * @var FormElementFactory
+     */
+    protected FormElementFactory $elementFactory;
+
+    /**
+     * @param TemplateContext $context
+     * @param FormElementFactory $elementFactory
+     * @param array $data
+     */
+    public function __construct(
+        TemplateContext $context,
+        FormElementFactory $elementFactory,
+        $data = []
+    ) {
+        $this->elementFactory = $elementFactory;
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * Prepare chooser element HTML
+     *
+     * @param Element $element
+     * @return Element
+     * @throws LocalizedException
+     */
+    public function prepareElementHtml(Element $element)
+    {
+        $config = $this->_getData('config');
+
+        $sourceUrl = $this->getUrl(
+            'cms/wysiwyg_images/index',
+            ['target_element_id' => $element->getId(), 'type' => 'file']
+        );
+        /** @var Button $chooser */
+        $chooser = $this->getLayout()->createBlock(\Magento\Backend\Block\Widget\Button::class)
+            ->setType('button')
+            ->setClass('btn-chooser')
+            ->setLabel($config['button']['open'])
+            ->setOnClick('MediabrowserUtility.openDialog(\'' . $sourceUrl . '\')')
+            ->setDisabled($element->getReadonly());
+
+        /** @var Text $input */
+        $input = $this->elementFactory->create("text", ['data' => $element->getData()]);
+        $input->setId($element->getId());
+        $input->setForm($element->getForm());
+        $input->setClass("widget-option input-text admin__control-text hidden");
+        if ($element->getRequired()) {
+            $input->addClass('required-entry');
+        }
+
+        $element->setData('after_element_html', $input->getElementHtml() . $chooser->toHtml());
+        return $element;
+    }
+}
